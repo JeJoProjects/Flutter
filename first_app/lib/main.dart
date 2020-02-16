134471c2068a9dd664feb4e55e0719d7641b7ca0
JeJo <email2jejo@gmail.com>
2020-02-16 20:34:23 +0100

    First App: buttons and questions

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,4 +1,45 @@
-void main()
-{
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer printed');
+  }
+
+  @override // declarator
+  Widget build(BuildContext buildContext) {
+    var questions = [
+      'What\'s your favourate color ?',
+      'What\'s your favourate animal ?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer - 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer - 2'),
+              onPressed: () => print('Answer 2 has been choosen'),
+            ),
+            RaisedButton(
+              child: Text('Answer - 3'),
+              onPressed: () {
+                // some other codes....
+                print('Answer 3 has been choosen');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
